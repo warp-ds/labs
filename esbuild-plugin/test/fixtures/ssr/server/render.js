9d03ad756036a8f2169338017d8c0c939c580bd9
William Killerud <william.killerud@schibsted.com>
2024-08-12 13:17:25 +0200

    chore: graduate esbuild-plugin
    Moved to https://github.com/warp-ds/esbuild-plugin

--- esbuild-plugin/test/fixtures/ssr/server/render.js
+++
@@ -1,9 +0,0 @@
-import { render as ssr } from "@lit-labs/ssr";
-import { html } from "lit";
-
-import "../client/components/component.js";
-
-export default function render() {
-  const results = [...ssr(html`<my-component></my-component>`)];
-  return results.join("");
-}
